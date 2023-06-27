6c8d1afe1f49bb35929f11e4abb37d0f84db63da
CuongNguyenhdkg91 <hoangkieumi2019@gmail.com>
2023-06-27 20:59:02 +0700

    change config tailwind medium screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+
+    screens: {
+      'md': '300px'
+    }
+
   },
   plugins: [],
 }
