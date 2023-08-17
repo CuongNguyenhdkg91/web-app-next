b04df50da2de78007b6c8fa4d68b98bf2555bb5a
CuongNguyenhdkg91 <hoangkieumi2019@gmail.com>
2023-08-17 21:07:42 +0700

    prisma

--- prisma/prismadb.ts
+++ prisma/prismadb.ts
@@ -8,4 +8,5 @@ declare global {
 const client = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
+//export {client as prisma}
 export default client
