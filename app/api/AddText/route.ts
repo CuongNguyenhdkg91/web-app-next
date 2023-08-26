7648d276cefd95b2b7654dd1fea8daa1ba48f8d6
CuongNguyenhdkg91 <hoangkieumi2019@gmail.com>
2023-08-26 21:47:30 +0700

    add grade field

---
+++ app/api/AddText/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../prisma/prismadb";
+
+export async function PUT(request:Request,) {
+    const body = await request.json()
+    const {id,title,grade} = body
+    
+    Object.keys(body).forEach((value: any) => {
+        if (!body[value]) {NextResponse.error()}
+      })
+
+      const result = await prisma.post.update({
+        where: {id},
+        data: {
+          title,
+          grade,
+        }
+      })
+
+      return NextResponse.json(result)
+    }
