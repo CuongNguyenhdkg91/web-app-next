551d89d733badef88b0477a03a93cc73bda6f26f
CuongNguyenhdkg91 <hoangkieumi2019@gmail.com>
2024-11-13 19:16:35 +0700

    basic MVC of Form

--- components/Form/FormAdd/FormAdd01.tsx
+++ components/Form/FormAdd/FormAdd01.tsx
@@ -1,11 +1,13 @@
 import { IoMdClose } from "react-icons/io";
-import { IconType } from "react-icons";
 
-const Button:React.FC<{label:string}> = ({label}) => {
+import { FocusEventHandler, MouseEventHandler, useState } from "react";
+import { FieldValues, UseFormRegister, useForm } from "react-hook-form";
+
+const Button:React.FC<{label:string, action?:MouseEventHandler}> = ({label, action}) => {
   return(
-      <button className="relative h-10 w-20 bg-[#ff66c4] rounded-lg font-['RocaOne'] text-[#004aad]
+      <button className="relative h-10 w-20 bg-[#ff66c4] rounded-lg 
       disabled:opacity-70 disabled:cursor-not-allowed hover:opacity-80 transition"
-      // onClick = {action}
+      onClick = {action}
       // disabled={disabled}
       >
           {/* {Icon && ( <Icon size={24} className="absolute left-4 top-3" />)} */}
@@ -17,14 +19,16 @@ const Button:React.FC<{label:string}> = ({label}) => {
 interface InputProps {
   id: string
   label: string;
+  register: UseFormRegister<FieldValues>
+  action?: FocusEventHandler
 }
 
-const Input: React.FC<InputProps> = ({id,label,}) => {
+const Input: React.FC<InputProps> = ({id,label,register,action}) => {
   return (
     <div className="w-full">
-        <label className="font-['AmsterdamOne'] text-[#004aad] pointer-events-none">{label}</label>            
-        <input id = {id} placeholder="text"
-          className="w-full p-2 pb-0.5 pt-5 font-light bg-white border rounded-2xl border-[#ff66c4] outline-none transition 
+        <label className="font-['AmsterdamOne'] pointer-events-none">{label}</label>            
+        <input id = {id} placeholder="text" {...register(id)} onBlur={action}
+            className="w-full p-2 pb-0.5 pt-5 bg-white border rounded-2xl border-[#ff66c4] outline-none transition 
                     disabled:opacity-70 disabled:cursor-not-allowed peer"
         />
     </div>
@@ -33,7 +37,8 @@ const Input: React.FC<InputProps> = ({id,label,}) => {
 
 export const FormAdd01: React.FC<{title:string, defaultImg: string}> = ({title, defaultImg}) => {
 
-    const imgSrc = defaultImg
+    const [imgSrc,setUrl] = useState(defaultImg)
+    const {watch, register} = useForm()
 
     return(
     <>
@@ -44,21 +49,26 @@ export const FormAdd01: React.FC<{title:string, defaultImg: string}> = ({title, 
                 <IoMdClose color="white" size={30} />
             </button>
 
-            <div id="WrapForm" className="flex gap-3 translate w-full max-w-[1000px] bg-[#cdffd8]
-                                          md:h-screen focus:outline-none">
-                <div id="WrapInput" className="p-5 w-1/2 flex flex-col justify-between gap-24">
+            <div id="WrapForm" className="flex gap-3 translate w-full max-w-[1000px] h-full max-h-[500px] font-['RocaOne'] text-[#004aad] focus:outline-none
+                                          bg-gradient-to-r from-[#cdffd8] to-[#94b9ff]">
+                <div id="WrapInput" className="p-5 w-1/2 shrink-0 flex flex-col justify-between">
+
                   {/*header*/}
                   <div className="w-fit justify-center rounded-t border-b-4 border-[#ff3131]">
-                    <div className="text-[24px] font-['RocaOne'] text-[#004aad]">
+                    <div className="text-[24px]">
                       {title}
                     </div>
                   </div>
   
                   {/*body*/}
                   <div className="">
-                      <div className='flex flex-col gap-4'>
-                          <Input id = 'url' label = 'Url' />
-                          <Input id = 'name' label = 'Description' />
+                      <div className='flex flex-col gap-8'>
+                          <Input id = 'url' label = 'Url' register={register} action= {()=>setUrl(watch("url"))}/>
+                          <Input id = 'name' label = 'Description' register={register}/>
+                          <div className="flex items-center gap-2">
+                              <div className="fa fa-check-square fa-2x"></div>
+                              <div className="text-[18px]">Option or not ?</div>
+                          </div>
                       </div>
                   </div>
 
@@ -66,7 +76,7 @@ export const FormAdd01: React.FC<{title:string, defaultImg: string}> = ({title, 
                   <div className=" w-full flex flex-row-reverse items-center justify-between gap-4">
                       <Button 
                         label="Add"
-                      //   action = {actSave}
+                        action = {()=>setUrl(watch("url"))}
                       />
                       <Button 
                         label="Update"
